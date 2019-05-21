684233e12047fe0445184225f1b341c8dac10d56
sanda0607 <sanda0607@163.com>
2019-05-21 10:58:36 +0800

    添加默认值

--- src/components/Editor/utils/createImage.js
+++ src/components/Editor/utils/createImage.js
@@ -1,6 +1,6 @@
-export default ({ name = '默认名称' })=>({
-	x:10,
-	y:10,
+export default ({ name = '默认名称',x=10,y=10 })=>({
+	x,
+	y,
 	w:52,
 	h:52,
 	name,
--- src/components/Editor/utils/createText.js
+++ src/components/Editor/utils/createText.js
@@ -1,13 +1,13 @@
-export default ({ text = '输入文字', name = '默认名称', fontSize= 16}) => ({
-    x: 10,
-    y: 10,
+export default ({ text = '输入文字', name = '默认名称', fontSize= 16,x=10,y=10,color ='#000'}) => ({
+    x,
+    y,
     // r: 0,
     // s: 1,
     name,
     text,
     fontSize,
     fontFamily: "微软雅黑",
-    color: "#000",
+    color,
     background: "#fff",
     id: 0
 });
